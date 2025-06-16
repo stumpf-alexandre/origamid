22c9cb7c803106361517a3f8d1942e12015b55f2
Alexandre <als.stumpf@gmail.com>
2025-06-15 22:24:17 -0300

    apenas com tratamento o array fica melhor apresentado

--- Modulo2/jsxComArrays/src/App.jsx
+++ Modulo2/jsxComArrays/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  const produtos = ['Notebook', 'Smartphone', 'Tablet'];
+  const produtos = ['Notebook, ', 'Smartphone, ', 'Tablet, '];
   return <p>{produtos}</p>;
 };
 
