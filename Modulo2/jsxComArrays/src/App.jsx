83f39566b949c02faed8c89de6e127ffafb6523a
Alexandre <als.stumpf@gmail.com>
2025-06-15 22:46:58 -0300

    desestruturando o array em outro local e passando mais dados

--- Modulo2/jsxComArrays/src/App.jsx
+++ Modulo2/jsxComArrays/src/App.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 
 const App = () => {
   const filmes = ['Before Sunrise', 'Before Sunset', 'Before Midnight'];
+
+  const livros = [
+    {nome: 'A Game of Thrones', ano: 1996 },
+    {nome: 'A Clash of Kings', ano: 1998 },
+    {nome: 'A Storm of Swords', ano: 2000 },
+  ];
   return (
+    <>
     <ul>
       {filmes.map((filme, index) => (
         <li key={filme}>{index} {filme}</li>
       ))}
     </ul>
+    <ul>
+      {livros.map(({nome, ano}) => (
+        <li key={nome}>{nome}, {ano}</li>
+      ))}
+    </ul>
+    </>
   );
 };
 
