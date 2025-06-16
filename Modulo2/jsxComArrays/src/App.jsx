fdf9974b2687191c02bbeca1e752a4203f49980e
Alexandre <als.stumpf@gmail.com>
2025-06-15 23:47:29 -0300

    exercicios com arrays e sua desestruturação

--- Modulo2/jsxComArrays/src/App.jsx
+++ Modulo2/jsxComArrays/src/App.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
+
 const App = () => {
   const filmes = ['Before Sunrise', 'Before Sunset', 'Before Midnight'];
 
@@ -8,6 +31,9 @@ const App = () => {
     {nome: 'A Clash of Kings', ano: 1998 },
     {nome: 'A Storm of Swords', ano: 2000 },
   ];
+
+  const dados = produtos.filter(({preco}) => Number(preco.replace('R$ ', '')) > 1500)
+
   return (
     <>
     <ul>
@@ -20,6 +46,16 @@ const App = () => {
         <li key={nome}>{nome}, {ano}</li>
       ))}
     </ul>
+    <hr />
+    {dados.map(({ id, nome, preco, cores}) => <div key={id}>
+      <h1>{nome}</h1>
+      <p>{preco}</p>
+      <ul>
+        {cores.map((cor) => (
+          <li key={cor} style={{ backgroundColor: cor, color: 'white' }}>{cor}</li>
+        ))}
+      </ul>
+    </div>)}
     </>
   );
 };
