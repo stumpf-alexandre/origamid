98578b553759b7c4a7e3f815df8a59a482512746
Alexandre <als.stumpf@gmail.com>
2025-06-17 23:00:41 -0300

    criando diversas formas de codigo de eventos

---
+++ Modulo2/eventos/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+//Podemos atribuir eventos diretamente aos elementos JSX como um atributo. Os eventos são sintáticos, ou seja, são criados pelo próprio React porém seguindo as especificações da W3C (e funcionam igualmente nos diversos browsers que o React suporta).
+
+const App = () => {
+  function handleClick(event) {
+    alert('Comprou: ' + event.target.innerText);
+  }
+  function handleScroll(event) {
+    alert(event);
+  }
+  window.addEventListener('scroll', handleScroll);
+  return (
+    <>
+      <div>
+        <button onClick={handleClick}/*evento do botão*/>Bermuda</button>
+      </div>
+      <div>
+        <button onMouseMove={handleClick}/*evento do mouse*/>Tênis</button>
+      </div>
+      <div style={{ height: '200vh' }}>
+        <button onClick={(event) => alert(event.target.innerText)}/*executar uma função anônima no evento expecifico ou pequeno*/>
+          Vamos Comprar
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default App;
