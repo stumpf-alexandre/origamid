3df68bc1caec3077c8a9ddd6572184f54d2c1329
Alexandre <als.stumpf@gmail.com>
2025-06-23 00:24:07 -0300

    iniciando codigo das propriedades

---
+++ Modulo2/propriedades/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Propriedades</h1>
+      <p>Assim como uma função pode receber argumentos, podemos também passar argumentos aos componentes. Esses são conhecidos como propriedades ou props.</p>
+    </>
+  );
+};
+
+export default App
