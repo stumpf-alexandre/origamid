fcca6f6106c1e079f8818570c58d18bc721d3203
Alexandre <als.stumpf@gmail.com>
2025-06-23 00:46:05 -0300

    adicionando props aos componentes

--- Modulo2/propriedades/src/App.jsx
+++ Modulo2/propriedades/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
 
+const Titulo = (props) => {
+  return <h1 style={{ color: props.cor }}>{props.text}</h1>;
+};
 const App = () => {
   return (
     <>
       <h1>Propriedades</h1>
-      <p>Assim como uma função pode receber argumentos, podemos também passar argumentos aos componentes. Esses são conhecidos como propriedades ou props.</p>
+      <p>
+        Assim como uma função pode receber argumentos, podemos também passar
+        argumentos aos componentes. Esses são conhecidos como propriedades ou
+        props.
+      </p>
+      <div>
+        <Titulo cor="red" text="Meu titulo 1" />
+        <Titulo cor="blue" text="Meu titulo 2" />
+        <Titulo cor="green" text="Meu titulo 3" />
+      </div>
     </>
   );
 };
 
-export default App
+export default App;
